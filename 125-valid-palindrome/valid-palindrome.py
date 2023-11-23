d90f7df28970d20477fc5c80fd2467430b134449
양시준 <yangsijun5528@gmail.com>
2023-11-23 20:14:28 +0900

    Time: 46 ms (77.52%) | Memory: 18.1 MB (15.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,11 +1,7 @@
-import collections
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
         s = s.lower()
         s = re.sub('[^a-z0-9]', '', s)
-
         return s == s[::-1]
 
 
