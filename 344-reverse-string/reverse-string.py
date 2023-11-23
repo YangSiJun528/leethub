a871adb06faed82cbeeac0b5ca0e48eeef05a279
양시준 <yangsijun5528@gmail.com>
2023-11-23 20:13:35 +0900

    Time: 191 ms (67.04%) | Memory: 20.9 MB (15.85%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,9 +4,7 @@ class Solution:
         right = len(s)-1
 
         while left < right:
-            temp = s[left]
-            s[left] = s[right]
-            s[right] = temp
+            s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
 
@@ -19,3 +17,20 @@ class Solution:
 #     def reverseString(self, s: List[str]) -> None:
 #         return s.reverse()
         
+
+# v2 - classic two point
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         left = 0
+#         right = len(s)-1
+
+#         while left < right:
+#             temp = s[left]
+#             s[left] = s[right]
+#             s[right] = temp
+#             left += 1
+#             right -= 1
+
+#         return s
+
+# 
