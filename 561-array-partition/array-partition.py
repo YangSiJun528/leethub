49f5fe98f4484b97e31e3dcd06a36a7ac6884e65
양시준 <yangsijun5528@gmail.com>
2023-11-29 20:06:34 +0900

    Time: 240 ms (58.04%) | Memory: 19.3 MB (46.78%) - LeetSync

---
+++ 561-array-partition/array-partition.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        answer = 0
+
+        for i in range(0,len(nums), 2):
+            answer += nums[i]
+
+        return answer
+
+
+# num / 2 = pair의 개수
+# 정렬한 상태에서 짝수 인덱스 값을 기준으로 실행하면 됨
