8a425c8ccdade127b11d704348b89b8ade2c7e81
양시준 <yangsijun5528@gmail.com>
2023-12-04 20:16:28 +0900

    Time: 555 ms (64.59%) | Memory: 49.2 MB (32.27%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import collections
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        q = collections.deque()
+        node = head
+        while node:
+            q.append(node.val)
+            node = node.next
+
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+        return True
