00c02b72680b6031dd6b69d29835be53bffe4036
양시준 <yangsijun5528@gmail.com>
2023-12-17 23:11:56 +0900

    Time: 74 ms (39.50%) | Memory: 17.4 MB (15.37%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,13 +1,16 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        if len(rooms) == 0:
+                return True
+
         visited = set()
         def dfs(cur_v):
             visited.add(cur_v)
-            print(cur_v, visited)
             for v in rooms[cur_v]:
                 if v not in visited:
                     dfs(v)
             return len(visited) == len(rooms)
         return dfs(0)
 
-# 생각해보나 dict 필요없음
+# 생각해보나 dict 필요없음 + 불필요한 print 제거
+# 예외 경우(rooms가 0일떄) 추가
