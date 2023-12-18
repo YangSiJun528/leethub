68dedb93b28187f2cf99d7536f8db9810c39d5aa
양시준 <yangsijun5528@gmail.com>
2023-12-18 12:10:25 +0900

    Time: 37 ms (61.09%) | Memory: 16.3 MB (51.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,16 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         cnt = 0
-        mem = {0:0, 1:1, 2:2}
+        mem = {-1:0, 0:0, 1:1, 2:2}
         def cnt_stairs(num):
-
-            if num < 1: # top 또는 넘어감
-                if num == 0: # num이 top에 위치함
-                    cnt += 1
-                return cnt
             if num not in mem:
                 mem[num] = cnt_stairs(num-1) + cnt_stairs(num-2)
             return mem[num]
+
         return cnt_stairs(n)
 
 # 2^45 = 3.5184372e+13 -> 완전탐색으로 불가능
