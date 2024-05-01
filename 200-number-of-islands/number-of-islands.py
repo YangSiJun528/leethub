f68140c06c7d7b9adeb75f7422710ae24308305b
양시준 <yangsijun5528@gmail.com>
2024-05-01 09:17:41 +0900

    Time: 263 ms (31.84%) | Memory: 23.5 MB (36.19%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -7,22 +7,22 @@ class Solution:
         len_n = len(grid[0])
         ways = [(-1,0), (1,0), (0,-1), (0,1)]
         visited = set()
-        q = collections.deque()
+        s = []
 
         for m in range(len_m):
             for n in range(len_n):
                 if grid[m][n] == '1' and (m, n) not in visited:
                     island_cnt += 1
                     # BFS Start
-                    q.append((m,n))
-                    while q:
-                        cur_m, cur_n = q.popleft()
+                    s.append((m,n))
+                    while s:
+                        cur_m, cur_n = s.pop()
                         if (cur_m,cur_n) not in visited:
                             visited.add((cur_m, cur_n))
                             for way in ways:
                                 next_m, next_n = way[0]+cur_m, way[1]+cur_n
                                 if next_m >= 0 and next_n >= 0 and next_m < len_m and next_n < len_n and grid[next_m][next_n] == '1':
-                                    q.append((next_m, next_n))
+                                    s.append((next_m, next_n))
         return island_cnt
 
         
