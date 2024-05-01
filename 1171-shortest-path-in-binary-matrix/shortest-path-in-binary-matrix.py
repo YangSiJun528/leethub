358dafe37e0e31a99cafd930bb5b2c57e233bf57
양시준 <yangsijun5528@gmail.com>
2024-05-01 09:43:59 +0900

    Time: 497 ms (33.40%) | Memory: 18.2 MB (18.84%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -2,31 +2,26 @@ import collections
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        valid_x = lambda x: x < len(grid) and x >= 0
-        valid_y = lambda y: y < len(grid) and y >= 0
-
-        if grid[0][0] == 1 or grid[-1][-1] == 1:
+        if grid[0][0] == 1:
             return -1
 
-        visited = set((0,0))
-        dx = [1, 1, 1, 0, 0, -1, -1, -1]
-        dy = [1, 0, -1, 1, -1, 1, 0, -1]
+        len_x = len(grid)
+        ways = [(1,1), (1,0), (1,-1), (-1,1), (0,1), (-1,-1), (-1,0), (0,-1)]
+        visited = set()
         q = collections.deque()
         q.append((0,0,1))
-        
+
         while q:
             cur_x, cur_y, cur_cnt = q.popleft()
-            if (cur_x, cur_y) == (len(grid)-1, len(grid)-1):
-                return cur_cnt
-                
-            for i in range(8):
-                next_x = dx[i] + cur_x
-                next_y = dy[i] + cur_y
-                if (
-                    valid_x(next_x) and valid_y(next_y) 
-                    and (next_x,next_y) not in visited
-                    and grid[next_x][next_y] == 0):
-                    q.append((next_x,next_y,cur_cnt+1))
-                    visited.add((next_x,next_y))
+            if (cur_x, cur_y) not in visited:
+                visited.add((cur_x, cur_y))
+                if (cur_x, cur_y) == (len_x-1,len_x-1):
+                    return cur_cnt
+                for way in ways:
+                    next_x, next_y, next_cnt = cur_x+way[0], cur_y+way[1], cur_cnt+1
+                    if next_x >= 0 and next_y >= 0 and next_x < len_x and next_y < len_x and grid[next_x][next_y] == 0:
+                        q.append((next_x, next_y, next_cnt))
         return -1
+        
+
         
