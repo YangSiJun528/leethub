90010d7828fbb4d6ab8a2bc37924e78739c6b024
양시준 <yangsijun5528@gmail.com>
2024-05-01 16:55:52 +0900

    Time: 36 ms (46.27%) | Memory: 16.5 MB (30.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cnt = 0
-        mem = {-1:0, 0:0, 1:1, 2:2}
-        for num in range(3, n+1):
-            if num not in mem:
-                mem[num] = mem[num-1] + mem[num-2]
-        return mem[n]
-
-# 2^45 = 3.5184372e+13 -> 완전탐색으로 불가능
-
-# 반복 - 값이 끝에 도달하면 cnt+1, 넘어가면 중지하되 cnt는 그대로, 끝에 도달하지 않으면 반복
-# DP는 결국 큰 문제를 작은 하위 문제로 나누는 형식이여야 함
+        dp = [-1, 1, 2, 3] + [-1] * (n - 3)
+        for i in range(4, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+        
