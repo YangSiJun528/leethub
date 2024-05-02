c9be7bd92812add379f33eb15cfa28f06ee107dc
양시준 <yangsijun5528@gmail.com>
2024-05-02 17:03:35 +0900

    Time: 389 ms (27.87%) | Memory: 18.7 MB (39.08%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,23 +1,25 @@
-import heapq, collections
+import collections, heapq
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = collections.defaultdict(list)
-        for time in times:
-            graph[time[0]].append((time[2], time[1]))
-            #graph[time[1]] - 현재 node가 바라보는? edge가 없을때, 빈 배열 설정
+        graph = collections.defaultdict(lambda: [])
+        for t in times:
+            graph[t[0]].append((t[1], t[2])) # path, cost
+        
         costs = {}
         pq = []
-        print(graph)
-        heapq.heappush(pq, (0, k))
+        cnt = 0
+        last = 0
+        heapq.heappush(pq, (0, k)) # cur_cost, start
 
         while pq:
-            cur_cost, cur_v = heapq.heappop(pq)
-            if cur_v not in costs:
-                costs[cur_v] = cur_cost
-                for cost, next_v in graph[cur_v]:
-                    next_cost = cur_cost + cost
-                    heapq.heappush(pq, (next_cost, next_v))
-        return max(costs.values()) if len(costs) == n else -1
-        
-# max 안쓰고 그냥 max_value 변수 만들고 갱신하는 식으로 해도 될 듯?
+            cost, node = heapq.heappop(pq)
+            if node not in costs:
+                costs[node] = cost
+                cnt += 1
+                last = cost
+                for next_node, next_cost in graph[node]:
+                    heapq.heappush(pq, (cost+next_cost, next_node))
+        if cnt != n:
+            return -1
+        return last
